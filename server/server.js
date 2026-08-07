de17ee42ab4eab0be5e0d1841fbcf4229b96a348
Ayush Sahu <ayush221003@gmail.com>
2026-08-08 01:28:23 +0530

    eigth

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ await connectDB();
 
 app.use(express.json());
 app.use(cors());
-app.use(clerkMiddleware)
+app.use(clerkMiddleware())
 
 app.get('/', (req, res)=> res.send('Server is running'))
 app.use("/api/inngest", serve({ client: inngest, functions }));
